40fb0b9f53c70d726db498d438265e002d12fc5f
chz <chz@bingularity.org>
2011-07-31 21:11:28 -0700

    Fix bintest processing to have more robust testdir extraction.

--- bintests/process.sh
+++ bintests/process.sh
@@ -5,18 +5,23 @@ if [ -z "$KMC_RUN_OUTPUTPATH" ]; then
 	KMC_RUN_OUTPUTPATH="bintests/out/"
 fi
 
-ls "$KMC_RUN_OUTPUTPATH"/*/klee-last/*.err
+function path2testdir
+{
+	testmd5=`echo "$1" | sed "s/\//\n/g" | grep "^[0-9a-f][0-9a-f][0-9a-f][0-9a-f]" | tail -n1`
+	testdir="$KMC_RUN_OUTPUTPATH"/$testmd5
+	echo "$testdir"
+}
 
 ERRXML="$KMC_RUN_OUTPUTPATH/err.xml"
 echo '<?xml version="1.0" encoding="ISO-8859-1"?>' >$ERRXML
 echo '<?xml-stylesheet type="text/xsl" href="err.xsl"?>' >>$ERRXML
 echo "<errors>" >>$ERRXML
 for a in "$KMC_RUN_OUTPUTPATH"/*/klee-last/*err; do
-	base=`echo $a | cut -f3 -d'/'`
+	testdir=`path2testdir "$a"`
 	echo "<error>"
 	echo "<errfile>"`echo "$a" | sed 's/\//\n/g' | tail -n1`"</errfile>"
 	echo -n "<cmdline>"
-	cat "$KMC_RUN_OUTPUTPATH"/$base/line 2>/dev/null
+	cat "$testdir"/line
 	echo "</cmdline>"
 	echo "<frames>"
 	grep Stack -A30 $a | grep "^[[:space:]]*#" | awk ' { print "<frame>"$0"</frame>" } '
@@ -46,7 +51,7 @@ echo asdasd
 # get runs that timed out
 
 for a in "$KMC_RUN_OUTPUTPATH"/*/timeout.txt; do
-	cat "$KMC_RUN_OUTPUTPATH"/`echo $a | cut -f3 -d'/'`/line 2>/dev/null
+	cat `path2testdir "$a"`/line 2>/dev/null
 done >"$KMC_RUN_OUTPUTPATH"/timeouts.txt
 
 # runs that died from the crappy solver
@@ -55,7 +60,7 @@ for a in "$KMC_RUN_OUTPUTPATH"/*/klee-last/warnings.txt; do
 	if [ -z "$x" ]; then
 		continue
 	fi
-	cat "$KMC_RUN_OUTPUTPATH"/`echo $a | cut -f3 -d'/'`/line 2>/dev/null
+	cat `path2testdir "$a"`/line
 done >"$KMC_RUN_OUTPUTPATH"/"badsolver.txt"
 
 for a in "$KMC_RUN_OUTPUTPATH"/*/stderr; do
@@ -63,7 +68,7 @@ for a in "$KMC_RUN_OUTPUTPATH"/*/stderr; do
 	if [ -z "$x" ]; then
 		continue
 	fi
-	cat "$KMC_RUN_OUTPUTPATH"/`echo $a | cut -f3 -d'/'`/line 2>/dev/null
+	cat `path2testdir "$a"`/line
 done >"$KMC_RUN_OUTPUTPATH"/"done.txt"
 
 for a in "$KMC_RUN_OUTPUTPATH"/*/stderr; do
@@ -71,7 +76,7 @@ for a in "$KMC_RUN_OUTPUTPATH"/*/stderr; do
 	if [ -z "$x" ]; then
 		continue
 	fi
-	cat "$KMC_RUN_OUTPUTPATH"/`echo $a | cut -f3 -d'/'`/line 2>/dev/null
+	cat `path2testdir "$a"`/line
 done >"$KMC_RUN_OUTPUTPATH"/"error.txt"
 
 for a in "$KMC_RUN_OUTPUTPATH"/*/stderr; do
@@ -79,7 +84,7 @@ for a in "$KMC_RUN_OUTPUTPATH"/*/stderr; do
 	if [ -z "$x" ]; then
 		continue
 	fi
-	cat "$KMC_RUN_OUTPUTPATH"/`echo $a | cut -f3 -d'/'`/line 2>/dev/null
+	cat `path2testdir "$a"`/line
 done >"$KMC_RUN_OUTPUTPATH"/"syscall.txt"
 
 for a in "$KMC_RUN_OUTPUTPATH"/*/stderr; do
@@ -87,11 +92,11 @@ for a in "$KMC_RUN_OUTPUTPATH"/*/stderr; do
 	if [ -z "$x" ]; then
 		continue
 	fi
-	cat "$KMC_RUN_OUTPUTPATH"/`echo $a | cut -f3 -d'/'`/line 2>/dev/null
+	cat `path2testdir "$a"`/line
 done >"$KMC_RUN_OUTPUTPATH"/"abort.txt"
 
 # xmlized stats
 for a in "$KMC_RUN_OUTPUTPATH"/*/klee-last; do
-	FDIR="$KMC_RUN_OUTPUTPATH"/`echo $a | cut -f3 -d'/'`
-	klee-stats --xml $a >$FDIR/last.stats
+	FDIR=`path2testdir "$a"`
+	klee-stats --xml "$a" >$FDIR/last.stats
 done
