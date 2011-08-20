141ce38d12d7735f12d45a67ef5853466fdee9b0
chz <chz@bingularity.org>
2011-08-20 02:11:39 -0700

    make config more flexible (remove hardcoded paths!)

--- setup.sh
+++ setup.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
+VEXLLVMDIR=${VEXLLVMDIR:-"/home/chz/src/vex/"}
 LLVMDIR=${LLVMDIR:-"/home/chz/src/llvm/llvm-2.6/"}
 STPDIR=${STPDIR:-"/home/chz/src/stp-fast/stp/"}
 BOOLECTORDIR=${BOOLECTORDIR:-"/home/chz/src/boolector/"}
 Z3DIR=${Z3DIR:-"/home/chz/src/z3/"}
 UCLIBDIR=${UCLIBDIR:-"/home/chz/src/klee-2.6-uclibc"}
+VEXLIBDIR=${VEXLIBDIR:-"/usr/lib/valgrind/"}
 
-CFLAGS="-g -O3 -I${STPDIR}/include"	\
-	./configure			\
-		--with-llvm="$LLVMDIR"	\
+LLVM_CFLAGS_EXTRA="$EXTRAHEADERS"			\
+LLVM_CXXFLAGS_EXTRA="$EXTRAHEADERS"			\
+CFLAGS="-g -O3 -I${STPDIR}/include $EXTRAHEADERS"	\
+CXXFLAGS="-g -O2  $EXTRAHEADERS"		\
+	./configure				\
+		--with-llvm="$LLVMDIR"		\
+		--with-libvex="$VEXLIBDIR"	\
+		--with-vexllvm="$VEXLLVMDIR"	\
 		--with-stp="$STPDIR"	\
 		--with-boolector="$BOOLECTORDIR"	\
 		--with-z3="$Z3DIR"			\
