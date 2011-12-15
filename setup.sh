212d0fd5dde09e6e419b3bb490e7d76d19e74975
chz <chz@bingularity.org>
2011-12-14 17:34:56 -0800

    fix dirpath

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ LLVMDIR=${LLVMDIR:-"/home/chz/src/llvm/llvm-3.0/"}
 STPDIR=${STPDIR:-"/home/chz/src/stp-fast/stp/"}
 BOOLECTORDIR=${BOOLECTORDIR:-"/home/chz/src/boolector/"}
 Z3DIR=${Z3DIR:-"/home/chz/src/z3/"}
-UCLIBDIR=${UCLIBDIR:-"/home/chz/src/klee-pruning"}
+UCLIBDIR=${UCLIBDIR:-"/home/chz/src/uclibc-pruning"}
 VEXLIBDIR=${VEXLIBDIR:-"/usr/lib/valgrind/"}
 
 LLVM_CFLAGS_EXTRA="$EXTRAHEADERS"			\
