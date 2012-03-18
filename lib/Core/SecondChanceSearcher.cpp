f60432d770ec1833dc3999a4ad4769f4eb9fd8e1
chz <chz@bingularity.org>
2012-03-17 17:11:17 -0700

    second chance searcher fixes

--- lib/Core/SecondChanceSearcher.cpp
+++ lib/Core/SecondChanceSearcher.cpp
@@ -20,38 +20,44 @@ SecondChanceSearcher::SecondChanceSearcher(Searcher* _searcher_base)
 , last_ins_cov(0)
 {}
 
+static uint64_t totalIns(void)
+{
+	return stats::coveredInstructions + stats::uncoveredInstructions;
+}
+
 ExecutionState& SecondChanceSearcher::selectState(bool allowCompact)
 {
 	assert (searcher_base->empty() == false);
 
 	if (last_current != NULL) {
-		if (last_ins_cov < stats::coveredInstructions) {
+		if (last_ins_cov < totalIns()) {
 			remaining_quanta += SecondChanceBoost;
-			last_ins_cov = stats::coveredInstructions;
+			last_ins_cov = totalIns();
+			std::cerr << "MORE. QUANTA=" << remaining_quanta << '\n';
 		}
 
 		if (remaining_quanta) {
 			remaining_quanta--;
+			std::cerr << "YOU GOT THE SECOND CHANCE ST="
+				<< (void*)last_current
+				<< ". QUANTA=" << remaining_quanta << '\n';
 			return *last_current;
 		}
 	}
 
 	remaining_quanta = 0;
 	last_current = &searcher_base->selectState(allowCompact);
+	last_ins_cov = totalIns();
 
 	return *last_current;
 }
 
 void SecondChanceSearcher::update(ExecutionState *current, States s)
 {
-	if (s.getRemoved().count(last_current))
+	if (s.getRemoved().count(last_current)) {
 		last_current = NULL;
-
-	if (	last_current != NULL &&
-		last_ins_cov < stats::coveredInstructions)
-	{
-		remaining_quanta += SecondChanceBoost;
-		last_ins_cov = stats::coveredInstructions;
+		last_ins_cov = totalIns();
+		remaining_quanta = 0;
 	}
 
 	searcher_base->update(current, s);
