6b5421db0329b772ee8d4c4297166644f717d24b
chz <chz@bingularity.org>
2013-12-05 22:24:09 -0800

    fix symargc replay test

--- test/MC/symargs_replay.c
+++ test/MC/symargs_replay.c
@@ -1,15 +1,17 @@
-// RUN: gcc %s -I../../../include/ -O0 -o %t1
+// RUN: gcc %s -O0 -o %t1
+// RUN: rm -rf guest-%t1
 // RUN: ../../../scripts/save_guest.sh ./%t1 guest-%t1
-// RUN: klee-mc -pipe-solver -symargs - ./%t1 asd 2>%t1.err >%t1.out
+// RUN: klee-mc -pipe-solver -symargc -symargs - ./%t1 2 aaaaa aaaaaa aaaaa >%t1.err >%t1.out
 // RUN: ls klee-last | not grep err
-// RUN: kmc-replay 1 >%t1.replay.out 2>&1
+// RUN: kmc-replay 1 2>%t1.replay.out
 // RUN: grep Exit %t1.replay.out
 // RUN: rm -rf guest-%t1
-#include <stdint.h>
+//
 int main(int argc, char* argv[])
 {
-	char	*arg = argv[1];
-	if (arg[1] == '0') puts("000");
-	if (arg[2] == '1') puts("111");
-	return 0;
+	if (argc == 0) return 1;
+	if (argc == 1) return 999;
+	if (argc == 2) return (argv[1] == '0') ? 11111 : 22222;
+	if (argc == 3) return 123123;
+	return 123;
 }
