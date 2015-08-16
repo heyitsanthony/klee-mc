e4b979243e42480c83f4704977923163e3b7a48f
chz <chz@bingularity.org>
2015-08-16 12:39:22 -0700

    test/mmu: relax the syscall stack corruption test to use earlyexit
    the mmu stack checking isn't too hot for syscalls, but I don't remmeber why
    
    earlyexits catch the funny behavior so at least there's that

--- test/MMU/mmu_stack_read_sc.c
+++ test/MMU/mmu_stack_read_sc.c
@@ -1,7 +1,8 @@
 // RUN: gcc %s -O0 -I../../../include/  -o %t1
-// RUN: klee-mc  -pipe-solver -use-sym-mmu -sym-mmu-type=../mmustack_stack.txt - ./%t1  2>%t1.err >%t1.out
+// RUN: klee-mc  -use-hookpass -hookpass-lib=earlyexits.bc -print-new-ranges  -pipe-solver -use-sym-mmu -sym-mmu-type=../mmustack_stack.txt - ./%t1  2>%t1.err >%t1.out
 // RUN: ls klee-last | grep decode.err$
-// RUN: ls klee-last | grep smash.warning
+// RUN: ls klee-last | egrep "(smash.warning|stackchk.err)"
+// RUN: ls klee-last | grep badjmp.err
 
 #include "klee/klee.h"
 #include <unistd.h>
@@ -11,7 +12,7 @@ int main(int argc, char *argv[])
 {
 	uint32_t	buf[4];
 
-	read(0, buf, sizeof(buf)*2);
+	read(0, buf, sizeof(buf) * 8);
 
 	/* test 2 */
 	return 3;
