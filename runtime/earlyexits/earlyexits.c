d0cf9e2f39d20a0990b5181171d9614219f2b2a0
chz <chz@bingularity.org>
2015-08-16 12:37:19 -0700

    runtime/earlyexit: add abort()
    shows up in bulk runs

--- runtime/earlyexits/earlyexits.c
+++ runtime/earlyexits/earlyexits.c
@@ -15,6 +15,8 @@ void __hookpre___assert_fail(void* regs)
 void __hookpre___GI___assert_fail(void* regs)
 { klee_uerror((const char*)GET_ARG0(regs), "uassert.err"); }
 
+void __hookpre___GI_abort(void* regs)
+{ klee_uerror("???", "abort.err"); }
 
 void __hookpre___GI_exit(void* regs) { exit(GET_ARG0(regs)); }
 
